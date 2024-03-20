371823813e768ae36208a30863b12642f473507c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-20 21:06:15 +0100

    base Model

--- models/base.py
+++ models/base.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ The Base Model for other classes/models """
 
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 
 Base = declarative_base()
 
-class BaseModel
+class BaseModel:
+    
