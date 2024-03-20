8cd363e85cb5a18f1f7052f02d5a350017f7cdce
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-20 21:48:13 +0100

    expense class

--- models/expense.py
+++ models/expense.py
@@ -8,6 +8,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 class Expense(BaseModel, Base):
     """ Defining the Expense Class """
     __tablename__ = 'expenses'
-    category_id = Column(String(60), ForeignKey('categories.id'), nullable=False)
+    category_id = Column(String(60), ForeignKey('categories.id'),
+                         nullable=False)
     amount = Column(Integer, nullable=False)
-    description = Column(String(128))
+    description = Column(String(128), nullable=True)
