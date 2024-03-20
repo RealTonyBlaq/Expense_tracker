5f799bc30e1c30b46392a8941382d4bd76342119
trust <isaactrust447@gmail.com>
2024-03-20 09:53:01 +0100

    my first commit

---
+++ baseModel/console.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import cmd
+
+class ExpenseCommand(cmd.cmd):
+
+
+
+if __name__='__main__':
+    ExpenseCommand().cmd.loop()
---
+++ baseModel/expense.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from datetime import datetime
+
+class Expense:
+    def __init__(self, name="", category="", amount=0.00, current_datetime=datetime.now()):
+       self.name = current_datetimename
+        self.category = category
+        self.amount = amount
+        self.current_datetime = current_datetime
+
