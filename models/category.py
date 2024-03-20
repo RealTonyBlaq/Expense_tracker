d5ca51bc842df9af00c0dc82cc7a02b098a7a454
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-20 21:37:23 +0100

    category class

--- models/category.py
+++ models/category.py
@@ -7,4 +7,5 @@ from sqlalchemy import Column, String, ForeignKey
 
 class Category(BaseModel, Base):
     """ Defining the class category for the expenses """
-    name = Column(String(60), )
+    name = Column(String(60), nullable=False)
+    user_id = Column(String(60), ForeignKey())
