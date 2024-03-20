3beef6d9119bba47f85e3c0f5dfaafe8ffc96932
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-20 21:28:02 +0100

    building user class

--- models/user.py
+++ models/user.py
@@ -8,4 +8,5 @@ from sqlalchemy import String, Integer, Column
 class User(BaseModel, Base):
     """ Defining the User class """
     first_name = Column(String(60), nullable=False)
-    last_name = Column
+    last_name = Column(String(60), nullable=False)
+    email = Column(String)
