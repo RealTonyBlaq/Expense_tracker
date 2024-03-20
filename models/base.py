7c8a8adbce07dab852d242a284b9897af1ed5a98
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-20 21:12:31 +0100

    base Model

--- models/base.py
+++ models/base.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ The Base Model for other classes/models """
 
-from datetime 
+from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 from uuid import uuid4
 
 
@@ -13,4 +13,5 @@ Base = declarative_base()
 class BaseModel:
     """ Defines the BaseModel class """
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(String(60), )
+    created_at = Column(DateTime, nullable=False)
+    updated_at
