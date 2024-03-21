bad56e0114155fb5a19b1722453dcaa3346f86d6
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-21 23:42:54 +0100

    fix

--- setup_mysql.sql
+++ setup_mysql.sql
@@ -1,4 +1,4 @@
--- Script sets up the storage for the Expense Tracker project
+-- Script sets up SQL for the Expense Tracker project
 CREATE DATABASE IF NOT EXISTS expense_tracker;
 CREATE USER IF NOT EXISTS 'et_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY et_dev_login;
 GRANT ALL PRIVILEGES ON expense_tracker.* TO 'et_dev'@'localhost';
