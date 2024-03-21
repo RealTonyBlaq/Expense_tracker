655403a5a5f8f7e51886eaa8acd85707993fc18f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-21 23:06:59 +0100

    storage model

--- models/engine/database.py
+++ models/engine/database.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ Storage Model """
 
-
+from models.base import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+class Database()
