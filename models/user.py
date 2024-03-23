5259c04a9a522e54053ed8daf845275da3166ec6
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-23 17:36:14 +0100

    adding relationship between user and category

--- models/user.py
+++ models/user.py
@@ -19,5 +19,5 @@ class User(BaseModel, Base):
     def details():
         """
         Returns a dictionary containing information about a user
-        including a list of dictionaries with ca
+        including a list of dictionaries with <category.name>.<>
         """
