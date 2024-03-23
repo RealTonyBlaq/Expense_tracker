6c5a378f418beecfa07a9f7565caf80363363218
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-23 18:10:39 +0100

    added relationship b/w category and expense

--- models/category.py
+++ models/category.py
@@ -3,6 +3,7 @@
 
 from models.base import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Category(BaseModel, Base):
@@ -10,3 +11,5 @@ class Category(BaseModel, Base):
     __tablename__ = "categories"
     name = Column(String(60), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    expenses = relationship('Expense', backref='categories',
+                            cascade='all, delete, delete-orphan')
