e26bd61cac5e8c0fcecb9f204869d2fb2208a1ff
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-25 21:00:54 +0100

    user route

--- api/routes/users.py
+++ api/routes/users.py
@@ -9,4 +9,6 @@ from models.user import User
 ETapp.route('/users', strict_slashes=False)
 ETapp.route('/users/<id>', strict_slashes=False)
 def retrieve(id=None):
-    """ Returns a dict with all user objects"""
+    """
+    Returns a dict with all user objects or a single user if id is not None
+    """
