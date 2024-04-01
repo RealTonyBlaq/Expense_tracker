cf1c7ec3becd75b51060ed5a1548936990bcdefe
trust <isaactrust447@gmail.com>
2024-04-01 17:26:48 +0100

    static files

--- baseModel/expense.py
+++ baseModel/expense.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
+"""This module defines a base class for all models in expense tracker"""
+
 from datetime import datetime
+from flask import Flask, render_template
+import uuid
+
 
 class Expense:
-    def __init__(self, name="", category="", amount=0.00, current_datetime=datetime.now()):
-       self.name = current_datetimename
+     """A base class for all expense tracker models"""
+
+    def __init__(self, name="", category="", amount=0.00, id=0):
+       self.name = name
         self.category = category
         self.amount = amount
-        self.current_datetime = current_datetime
+        id = str(uuid.uuid4())
 
+        
+    app = Flask (__name__)
+
+    @app.route('/')
+    @app.route('/home')
+    def home():
+        return render_template(home.html)
