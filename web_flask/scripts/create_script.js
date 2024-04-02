c2161f206c9c0cc094ddf8f440301615378d03df
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-02 23:17:37 +0100

    script for the create account page

---
+++ web_flask/scripts/create_script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $('#accountForm').submit(function (event) {
+    event.preventDefault();
+    const firstName = $('#firstName').val();
+    const lastName = $('#lastName').val();
+    const email = $('#email').val();
+    const password = $('#password').val();
+    $.ajax({
+      url: 'http://127.0.0.1:5000/api/users',
+      method: "POST",
+      data: JSON.stringify({
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        password: password
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      success: function () {
+        alert('New account created successfully');
+        window.location.href = "signin.html";
+      },
+      error: function (xhr, status, error) {
+        alert(xhr.responseText);
+      }
+    });
+  });
+});
