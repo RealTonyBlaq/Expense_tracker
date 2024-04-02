758ccfa36ed4c1268844e2ec02c91511c45457b3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-02 22:35:46 +0100

    added CORS

--- api/app.py
+++ api/app.py
@@ -7,10 +7,12 @@ from models.category import Category
 from models.expense import Expense
 from models import storage
 from models.user import User
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(ETapp)
+CORS(app)
 
 
 @app.teardown_appcontext
@@ -41,4 +43,4 @@ def status():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
