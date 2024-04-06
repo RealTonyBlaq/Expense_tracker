c0c57d284c882b9d0a050229d36756529038a0ac
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-06 14:36:42 +0100

    updated import for web_flask.app

--- api/__init__.py
+++ api/__init__.py
@@ -10,3 +10,4 @@ ETapp = Blueprint('ETapp', __name__, url_prefix='/api')
 from api.routes.categories import *
 from api.routes.users import *
 from api.routes.expenses import *
+from web_flask.app import *
