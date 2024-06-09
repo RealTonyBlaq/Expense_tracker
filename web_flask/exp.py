2904898404bf12de790728e3c546dbda94b4b6ab
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-09 19:20:17 +0100

    update, made some fixes

--- web_flask/exp.py
+++ web_flask/exp.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
 """ Expense Tracker Flask app """
 
 from flask import Flask, jsonify, redirect, request, render_template, url_for
+from flask_cors import CORS
 from api import ETapp
 from models import storage
 from models.category import Category
 from models.expense import Expense
 from models.user import User, confirm_account, check_string
-from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -35,55 +34,64 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/signup', strict_slashes=False)
+@app.route('/reset')
+def reset_password():
+    """ Returns the reset password page """
+    return render_template('home.html')
+
+
+@app.route('/dashboard', methods=['GET', 'POST'], strict_slashes=False)
+def dashboard():
+    """ Handles dashboard view and salary submission """
+    # if not g.user:
+    #    return redirect(url_for('signin'))
+
+    user_id = request.args.get('u')
+    return render_template('dashboard.html')
+
+    # return render_template('dashboard.html', salary=g.user.salary)
+
+
+@app.route('/signup', methods=['POST', 'GET'], strict_slashes=False)
 def signup():
-    return render_template("create.html")
-
-
-@app.route('/signin', strict_slashes=False)
-def signin():
-    return render_template("signin.html")
-
-
-@app.route('/dashboard', methods=['GET', 'POST'],
-           strict_slashes=False)
-def submit_salary():
-    if request.method == 'POST':
-        salary = request.form.get('salary_amount')
-        return render_template('dashboard.html', salary=salary)
-    else:
-        return render_template('dashboard.html')
-
-
-@app.route('/submit', methods=['POST', 'GET'],
-           strict_slashes=False)
-def submit_form():
     """ Handles the case when the user clicks on submit """
-    first_name = request.form.get('firstName')
-    last_name = request.form.get('lastName')
-    email = request.form.get('email')
-    password = request.form.get('password')
-    data = {
-        "first_name": first_name,
-        "last_name": last_name,
-        "email": email,
-        "password": password
-    }
-    new = User(**data)
-    new.save()
-    return render_template('signin.html', success="Account created successfully.. Please signin")
+    if request.method == 'POST':
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Add validation for input data here
+
+        data = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "password": password
+        }
+        new = User(**data)
+        new.save()
+        return render_template('signin.html',
+                               success="Account created successfully")
+
+    return render_template('create.html')
 
 
-@app.route('/login', methods=['POST', 'GET'],
-           strict_slashes=False)
-def login():
+@app.route('/signin', methods=['POST', 'GET'], strict_slashes=False)
+def signin():
     """ Confirms a User's details and logs them in to their dashboard """
-    email = request.form.get('email')
-    password = request.form.get('password')
-    user = confirm_account(email, password)
-    if user:
-        return redirect(url_for('dashboard'))
-    return render_template('signin.html', error='Incorrect email or password. Please try again!')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = confirm_account(email, password)
+        if user:
+            # g.user = user.details()
+            return redirect(url_for('dashboard', u=user.id))
+        return render_template('signin.html',
+                               error='Incorrect email/password. \
+                                   Please try again!')
+
+    return render_template('signin.html')
 
 
 if __name__ == "__main__":
