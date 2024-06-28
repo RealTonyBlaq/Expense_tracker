6b8e8cd307a2d0b0a542b316ce4e96dc43ad1636
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-28 01:02:10 +0100

    fix

--- models/user.py
+++ models/user.py
@@ -4,9 +4,8 @@
 from models.base import Base, BaseModel
 from models.category import Category
 from models.expense import Expense
-from sqlalchemy import String, Column, Integer
+from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
-from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -16,16 +15,8 @@ class User(BaseModel, Base):
     last_name = Column(String(60), nullable=False)
     email = Column(String(60), nullable=False, unique=True)
     password = Column(String(60), nullable=False)
-    salary = Column(Integer, default=0)
     categories = relationship("Category", backref="users",
                               cascade="all, delete, delete-orphan")
-
-    def set_salary(self, value):
-        """ Sets the salary for a User """
-        if isinstance(value, int) and value > 0:
-            setattr(self, 'salary', value)
-            self.save()
-        raise TypeError('Salary must be an Integer > 0')
 
     def details(self):
         """
@@ -52,33 +43,3 @@ class User(BaseModel, Base):
                         new[key].append(expense.about())
                 user_dict["expenses"].append(new)
         return user_dict
-
-
-def confirm_account(email, password):
-    """ Confirms and retrieves a User account """
-    from models import storage
-    if email and password:
-        user = storage.get_user(email)
-        if user:
-            pb = password.encode('utf-8')
-            pwd_hash = md5()
-            pwd_hash.update(pb)
-            pwd = pwd_hash.hexdigest()
-            if user.password == pwd:
-                return user
-            return None
-    return None    
-
-
-def check_string(string):
-    """
-    Checks a string for special characters
-
-    Return: Bool
-    """
-    char_list =  ['!', '@', '#', '.', '$', '%', '^', '&',
-                  '*', '(', ')', '/', '[', ']', '{', '}']
-    for char in char_list:
-        if char in string:
-            return True
-    return False
