a477fedc282885428e994c2c9a4577cd720cbed3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-15 22:10:25 +0100

    modified

--- web_flask/static/scripts/create_script.js
+++ web_flask/static/scripts/create_script.js
@@ -1,178 +1,94 @@
 $(document).ready(function () {
-    $('#email').on('keyup', function() {
+    function setStatus(element, icon, message, color, marginRight) {
+        $(element).html(`${icon} &nbsp; ${message}`).css({
+            'color': color,
+            'border': 'none',
+            'font-size': '12px',
+            'margin-right': marginRight
+        });
+    }
+
+    function setBorder(element, color) {
+        $(element).css({
+            'border': `1px solid ${color}`
+        });
+    }
+
+    function validateName(name, element, errorElement) {
+        const regex = /[!@#$%^&.*(){}]/;
+        if (regex.test(name)) {
+            setStatus(errorElement, '<i class="fas fa-exclamation-triangle"></i>', 'Name must contain only alphabets', 'red', '150px');
+            setBorder(element, 'red');
+            return false;
+        } else {
+            $(errorElement).empty();
+            setBorder(element, '#add');
+            return true;
+        }
+    }
+
+    $('#email').on('keyup', function () {
         const email = $(this).val();
-        // Send AJAX request to check email availability
         if (email.includes('@') && email.includes('.')) {
             $.ajax({
                 url: `http://127.0.0.1:5000/api/users/email/${email}`,
                 method: 'GET',
                 dataType: 'json',
-                success: function(response) {
-                    $('#emailAvailability').html('<i class="fas fa-exclamation-triangle"></i> &nbsp; email already exists').css({
-                        'color': 'red',
-                        'border': 'none',
-                        'font-size': '12px',
-                        'margin-right': '160px'
-                      });
-                    $('#email').css({
-                        'border': '1px solid red'
-                    });
+                success: function (response) {
+                    setStatus('#emailAvailability', '<i class="fas fa-exclamation-triangle"></i>', 'Email already exists', 'red', '160px');
+                    setBorder('#email', 'red');
                     $('#submitBtn').prop('disabled', true);
                 },
-                error: function(xhr, status, error) {
+                error: function (xhr) {
                     if (xhr.status === 404) {
-                        $('#emailAvailability').html('<i class="fas fa-check-circle"></i> &nbsp; email available').css({
-                            'color': 'green',
-                            'border': 'none',
-                            'font-size': '12px',
-                            'margin-right': '180px'
-                        });
-                        $('#email').css({
-                            'border': '1px solid green'
-                        });
+                        setStatus('#emailAvailability', '<i class="fas fa-check-circle"></i>', 'Email available', 'green', '180px');
+                        setBorder('#email', 'green');
                         $('#submitBtn').prop('disabled', false);
                     } else {
-                        $('#emailAvailability').html('<i class="fas fa-exclamation-triangle"></i> &nbsp; Error occurred while checking email availability').css({
-                            'color': 'red',
-                            'border': 'none',
-                            'font-size': '10px',
-                            'margin-right': '30px'
-                          });
-                        $('#email').css({
-                            'border': '1px solid red'
-                        });
+                        setStatus('#emailAvailability', '<i class="fas fa-exclamation-triangle"></i>', 'Error occurred while checking email availability', 'red', '30px');
+                        setBorder('#email', 'red');
                         $('#submitBtn').prop('disabled', true);
                     }
                 }
             });
         }
     });
+
     $('#password').on('keyup', function () {
-        const pwd = $('#password').val();
-        if (/[a-z]/.test(pwd)) {
-            $('#smallLetter').html('<i class="fas fa-check-circle"></i> &nbsp; small alphabets').css({
-                'color': 'green',
-                'border': 'none',
-                'font-size': '12px',
-                'margin-right': '180px'
-            });
-        } else {
-            $('#smallLetter').html('<i class="fas fa-exclamation-triangle"></i> &nbsp; Small alphabets').css({
-                'color': 'red',
-                'border': 'none',
-                'font-size': '12px',
-                'margin-right': '180px'
-            });
-        }
-        if (/[A-Z]/.test(pwd)) {
-            $('#capLetter').html('<i class="fas fa-check-circle"></i> &nbsp; At least one Capital letter').css({
-                'color': 'green',
-                'border': 'none',
-                'font-size': '12px',
-                'margin-right': '125px'
-            });
-        } else {
-            $('#capLetter').html('<i class="fas fa-exclamation-triangle"></i> &nbsp; At least one Capital letter').css({
-                'color': 'red',
-                'border': 'none',
-                'font-size': '12px',
-                'margin-right': '125px'
-            });
-        }
-        if (/[0-9]/.test(pwd)) {
-            $('#number').html('<i class="fas fa-check-circle"></i> &nbsp; At least one digit').css({
-                'color': 'green',
-                'border': 'none',
-                'font-size': '12px',
-                'margin-right': '175px'
-            });
-        } else {
-            $('#number').html('<i class="fas fa-exclamation-triangle"></i> &nbsp; At least one digit').css({
-                'color': 'red',
-                'border': 'none',
-                'font-size': '12px',
-                'margin-right': '175px'
-            });
-        }
-        if (/[!@#$%^&*()]/.test(pwd)) {
-            $('#specialCharacter').html('<i class="fas fa-check-circle"></i> &nbsp; At least one special character [!,@,#,$,%,^,&,*,(,)]').css({
-                'color': 'green',
-                'border': 'none',
-                'font-size': '12px',
-                'margin-right': '100px'
-            });
-        } else {
-            $('#specialCharacter').html('<i class="fas fa-exclamation-triangle"></i> &nbsp; At least one special character [!,@,#,$,%,^,&,*,(,)]').css({
-                'color': 'red',
-                'border': 'none',
-                'font-size': '12px',
-                'margin-right': '100px'
-            });
-        }
-        if (pwd.length >= 8) {
-            $('#pwdLength').html('<i class="fas fa-check-circle"></i> &nbsp; Password length at least 8 characters').css({
-                'color': 'green',
-                'border': 'none',
-                'font-size': '12px',
-                'margin-right': '50px'
-            });
-        } else {
-            $('#pwdLength').html('<i class="fas fa-exclamation-triangle"></i> &nbsp; Password length at least 8 characters').css({
-                'color': 'red',
-                'border': 'none',
-                'font-size': '12px',
-                'margin-right': '50px'
-            });
-        }
+        const password = $(this).val();
+        const criteria = [
+            { regex: /[a-z]/, element: '#smallLetter', message: 'small alphabets', marginRight: '180px' },
+            { regex: /[A-Z]/, element: '#capLetter', message: 'At least one Capital letter', marginRight: '125px' },
+            { regex: /[0-9]/, element: '#number', message: 'At least one digit', marginRight: '175px' },
+            { regex: /[!@#$%^&*()]/, element: '#specialCharacter', message: 'At least one special character [!,@,#,$,%,^,&,*,(,)]', marginRight: '100px' },
+            { regex: /.{8,}/, element: '#pwdLength', message: 'Password length at least 8 characters', marginRight: '50px' }
+        ];
+
+        criteria.forEach(criterion => {
+            if (criterion.regex.test(password)) {
+                setStatus(criterion.element, '<i class="fas fa-check-circle"></i>', criterion.message, 'green', criterion.marginRight);
+            } else {
+                setStatus(criterion.element, '<i class="fas fa-exclamation-triangle"></i>', criterion.message, 'red', criterion.marginRight);
+            }
+        });
     });
 
     $('#submitBtn').on('click', function (event) {
-      const lname = $('#lastName').val();
-      const fname = $('#firstName').val();
-      const pwd = $('#password').val();
-      if (/[!@#$%^&.*(){}]/.test(lname)) {
-        event.preventDefault();
-        $('#checkLastName').html('<i class="fas fa-exclamation-triangle"></i> &nbsp; Last name must contain only alphabets').css({
-          'color': 'red',
-          'border': 'none',
-          'font-size': '10px',
-          'margin-right': '150px'
-        });
-        $('#lastName').css({
-            'border': '1px solid red'
-        });
-      } else {
-        $('#checkLastName').remove();
-        $('#lastName').css({
-            'border': '1px solid #add'
-        });
-      }
-      if (/[!@#$%^&.*(){}]/.test(fname)) {
-        event.preventDefault();
-        $('#checkFirstName').html('<i class="fas fa-exclamation-triangle"></i> &nbsp; First name must contain only alphabets').css({
-            'color': 'red',
-            'border': 'none',
-            'font-size': '10px',
-            'margin-right': '150px'
-        });
-        $('#firstName').css({
-            'border': '1px solid red'
-        });
-      } else {
-        $('#checkFirstName').remove();
-        $('#firstName').css({
-            'border': '1px solid #add'
-        });
-      }
-      if (/[a-z]/.test(pwd) && /[A-Z]/.test(pwd) && /[0-9]/.test(pwd) && /[!@#$%^&*()]/.test(pwd) && pwd.length >= 8) {
-        $('#password').css({
-            'border': '1px solid green'
-        });
-      } else {
-        event.preventDefault();
-        $('#password').css({
-            'border': '1px solid red'
-        });
-      }
+        const lastName = $('#lastName').val();
+        const firstName = $('#firstName').val();
+        const password = $('#password').val();
+
+        const isLastNameValid = validateName(lastName, '#lastName', '#checkLastName');
+        const isFirstNameValid = validateName(firstName, '#firstName', '#checkFirstName');
+        const isPasswordValid = /[a-z]/.test(password) && /[A-Z]/.test(password) && /[0-9]/.test(password) && /[!@#$%^&*()]/.test(password) && password.length >= 8;
+
+        if (!isLastNameValid || !isFirstNameValid || !isPasswordValid) {
+            event.preventDefault();
+            if (!isPasswordValid) {
+                setBorder('#password', 'red');
+            }
+        } else {
+            setBorder('#password', 'green');
+        }
     });
 });
