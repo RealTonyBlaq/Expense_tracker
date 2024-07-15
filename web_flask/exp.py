10c0e1b4ef365feee5066a4bd33c83ba17beea0b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-15 22:10:23 +0100

    modified

--- web_flask/exp.py
+++ web_flask/exp.py
@@ -1,21 +1,41 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ Expense Tracker Flask app """
 
-from flask import Flask, jsonify, redirect, request, render_template, url_for
+from auth.session import Auth
+from flask import (abort, Flask, jsonify, make_response,
+                   redirect, request,
+                   render_template, url_for)
 from flask_cors import CORS
 from api import ETapp
+import os
 from models import storage
-from models.category import Category
-from models.expense import Expense
-from models.user import User, confirm_account, check_string
 
 
 app = Flask(__name__)
 app.register_blueprint(ETapp)
 app.static_folder = 'static'
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+auth = Auth()
 
 
+@app.before_request
+def before_all_requests() -> None:
+    #Executed before all other requests
+    if auth:
+        if auth.require_auth(request.path, ['/api/*',
+                                            '/static/*',
+                                            '/reset/',
+                                            '/signin/',
+                                            '/signup/',
+                                            '/',
+                                            '/home/']):
+            if not auth.session_cookie(request):
+                abort(401)
+            if not auth.current_user(request):
+                abort(403)
+            request.current_user = auth.current_user(request)
+
+    
 @app.teardown_appcontext
 def shutdown(error=None):
     """ Closes a Database session """
@@ -23,9 +43,21 @@ def shutdown(error=None):
 
 
 @app.errorhandler(404)
-def not_found(error):
+def not_found(error) -> str:
     """ Returns a JSON if a request route wasn't found """
     return jsonify({'error': 'Not Found'})
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Returns a JSON if the request wasn't authorized """
+    return jsonify({'error': 'unauthorized'})
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Returns a JSON if the request is forbidden """
+    return jsonify({'error': 'forbidden'})
 
 
 @app.route('/', strict_slashes=False)
@@ -37,19 +69,16 @@ def home():
 @app.route('/reset')
 def reset_password():
     """ Returns the reset password page """
-    return render_template('home.html')
+    return render_template('reset.html')
 
 
-@app.route('/dashboard', methods=['GET', 'POST'], strict_slashes=False)
+@app.route('/dashboard', strict_slashes=False)
 def dashboard():
     """ Handles dashboard view and salary submission """
-    # if not g.user:
-    #    return redirect(url_for('signin'))
-
-    user_id = request.args.get('u')
-    return render_template('dashboard.html')
-
-    # return render_template('dashboard.html', salary=g.user.salary)
+    user = auth.current_user(request)
+    if user:
+        return render_template('dashboard.html')
+    return redirect(url_for('home'))
 
 
 @app.route('/signup', methods=['POST', 'GET'], strict_slashes=False)
@@ -61,16 +90,13 @@ def signup():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        # Add validation for input data here
-
         data = {
             "first_name": first_name,
             "last_name": last_name,
             "email": email,
             "password": password
         }
-        new = User(**data)
-        new.save()
+        auth.create_user(data)
         return render_template('signin.html',
                                success="Account created successfully")
 
@@ -83,15 +109,14 @@ def signin():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        user = confirm_account(email, password)
-        if user:
-            # g.user = user.details()
-            return redirect(url_for('dashboard', u=user.id))
-        return render_template('signin.html',
-                               error='Incorrect email/password. \
-                                   Please try again!')
+        if auth.validate(email, password):
+            cookie = auth.create_session(email)
+            response = make_response(redirect(url_for('dashboard')), 200)
+            response.set_cookie('session_id', cookie)
+            return response
+        abort(401)
 
-    return render_template('signin.html')
+    return render_template('signin_page.html')
 
 
 if __name__ == "__main__":
