753477bd4d10b961bea3502e8c81f39e4e2566c3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-07-15 22:10:19 +0100

    modified

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """ The User Model """
 
 from models.base import Base, BaseModel
-from models.category import Category
+from models.income import Income
 from models.expense import Expense
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
@@ -15,8 +15,10 @@ class User(BaseModel, Base):
     last_name = Column(String(60), nullable=False)
     email = Column(String(60), nullable=False, unique=True)
     password = Column(String(60), nullable=False)
-    categories = relationship("Category", backref="users",
+    incomes = relationship("Income", backref="users",
                               cascade="all, delete, delete-orphan")
+    expenses = relationship('Expense', backref='users',
+                            cascade="all, delete, delete-orphan")
 
     def details(self):
         """
@@ -25,21 +27,20 @@ class User(BaseModel, Base):
         as key and a list of expenses as the value pair
         """
         from models import storage
-        categories = storage.all(Category)
+        incomes = storage.all(Income)
         expenses = storage.all(Expense)
         user_dict = {}
         for key, value in self.__dict__.items():
             if key not in ["password", 'categories', '_sa_instance_state']:
                 user_dict[key] = value
-        user_dict['categories'] = []
+        user_dict['incomes'] = []
         user_dict['expenses'] = []
-        for category in categories.values():
-            if category.user_id == self.id:
-                user_dict["categories"].append(category.about())
-                key = "{}.{}".format(category.name, category.id)
-                new = {key: []}
-                for expense in expenses.values():
-                    if expense.category_id == category.id:
-                        new[key].append(expense.about())
-                user_dict["expenses"].append(new)
+        for income in incomes.values():
+            if income.user_id == self.id:
+                user_dict["incomes"].append(income.about())
+
+        for expense in expenses.values():
+            if expense.user_id == self.id:
+                user_dict["expenses"].append(expense.about())
+
         return user_dict
