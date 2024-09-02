fd875c3bf268969755aa319cbf0dac85e3b28c17
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-02 22:52:22 +0100

    update

--- setup_mysql.sql
+++ setup_mysql.sql
@@ -1,4 +1,5 @@
 -- Script sets up SQL for the Expense Tracker project
+DROP DATABASE IF EXISTS expense_tracker;
 CREATE DATABASE IF NOT EXISTS expense_tracker;
 CREATE USER IF NOT EXISTS 'et_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'et_dev_login';
 GRANT ALL PRIVILEGES ON expense_tracker.* TO 'et_dev'@'localhost';
