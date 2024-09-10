85ae4e39164ae92af69440ebbd6b9cd17781c9aa
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-10 22:07:56 +0100

    expense model

--- models/expense.py
+++ models/expense.py
@@ -2,15 +2,16 @@
 """ The Expense Model """
 
 from models.base import Base, BaseModel
-from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy import Column, String, ForeignKey, Date, DECIMAL
+from sqlalchemy.dialects.sqlite import TEXT
+from sqlalchemy.orm import relationship
 
 
 class Expense(BaseModel, Base):
-    """ Defining the Expense Class """
+    """ The Expense Class holds the expenses made by the User """
     __tablename__ = 'expenses'
-    name = Column(String(60), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'),
                          nullable=False)
-    amount = Column(Integer, nullable=False)
+    amount = Column(DECIMAL(10, 2), nullable=False)
     date_occurred = Column(Date, nullable=False)
-    description = Column(String(128), nullable=True)
+    description = Column(TEXT, nullable=True)
