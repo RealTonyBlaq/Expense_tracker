8c0b83c41e2d0f6899681bad7caf4a517b6d1e80
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-10 23:53:50 +0100

    recurring expense

--- models/recurring_expense.py
+++ models/recurring_expense.py
@@ -3,5 +3,13 @@
 
 from models.base import BaseModel, Base
 from models.user import User
-from sqlalchemy import Column, String, DECIMAL, ForeignKey, DATE
+from sqlalchemy import Column, String, DECIMAL, ForeignKey, DATE, Enum
+from sqlalchemy.dialects.sqlite import TEXT
 from sqlalchemy.orm import relationship
+
+
+class RecurringExpense(BaseModel, Base):
+    """ Defines a model for recurring expenses """
+    __tablename__ = 'recurring_expenses'
+
+    
