e05963fa1c07cef672da03e6eac33707192211a4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-10 23:50:46 +0100

    recurring expense

--- models/recurring_expense.py
+++ models/recurring_expense.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 """ The Recurring Expense Model """
 
-from
+from models.base import BaseModel, Base
+from models.user import User
+from sqlalchemy import Column, String, DECIMAL, ForeignKey, DATE
+from sqlalchemy.orm import relationship
