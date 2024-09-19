565e2aa6062f17fb5a9dcfb49c1e9f25acdbae24
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-19 21:56:05 +0100

    update

--- models/category.py
+++ models/category.py
@@ -2,8 +2,6 @@
 """ The Category Model """
 
 from models.base import BaseModel, Base
-from models.user import User
-from models.expense import Expense
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -11,7 +9,6 @@ from sqlalchemy.orm import relationship
 class Category(BaseModel, Base):
     """ Category Model that groups Expense objects """
     __tablename__ = 'categories'
-
     name = Column(String(60), nullable=False)
     user_id = Column(String(60),
                      ForeignKey('users.id', ondelete='CASCADE'),
