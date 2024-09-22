bcc6658e66da3418dcf4a05fbd7b39b4f811cc25
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-22 21:33:07 +0100

    fixed the secondary table

--- models/associations.py
+++ models/associations.py
@@ -2,16 +2,14 @@
 """ The association tables """
 
 from models.base import Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Table
 
 
-class ExpenseTag(Base):
-    """ Association table for the Expense Table and Tag table """
-    __tablename__ = 'expense_tag'
-
-    expense_id = Column(String(60),
-                        ForeignKey('expenses.id', ondelete='CASCADE'),
-                        primary_key=True)
-    tag_id = Column(String(60),
-                    ForeignKey('tags.id', ondelete='CASCADE'),
-                    primary_key=True)
+expense_tag = Table(
+    "ExpenseTag",
+    Base.metadata,
+    Column('expense_id', String(60),
+           ForeignKey('expenses.id', ondelete='CASCADE'), primary_key=True),
+    Column('tag_id', String(60),
+           ForeignKey('tags.id', ondelete='CASCADE'), primary_key=True)
+)
