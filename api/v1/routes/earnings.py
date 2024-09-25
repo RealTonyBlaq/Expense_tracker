0c540b6e50728377723611034423c8da293eb01a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-25 19:34:55 +0100

    earnings route

--- api/v1/routes/earnings.py
+++ api/v1/routes/earnings.py
@@ -17,12 +17,8 @@ date_format = "%Y-%m-%d"
 @ETapp.route('/earnings/<earning_id>', strict_slashes=False)
 def get_earnings(earning_id=None):
     """ Returns an object containing a list of Earning objects """
-    if request.is_json:
         if earning_id:
             earning = db.query(Earning).filter_by(id = earning_id).first()
             if earning is not None:
                 return jsonify({'message': earning.to_dict()}), 200
             abort(404)
-
-        fo
-    return jsonify({'message': 'Not a valid JSON'}), 400
