4d2a8f97da1a3b0d3cf6154f181da1effad7ae08
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-25 19:13:39 +0100

    earnings route

--- api/v1/routes/earnings.py
+++ api/v1/routes/earnings.py
@@ -2,9 +2,11 @@
 """ The Earning route """
 
 from api.v1 import ETapp
-from flask_login import login_required
+from flask_login import login_required, current_user
 from models.earning import Earning
 
 
 @login_required
-@ETapp.route('')
+@ETapp.route('/earnings', strict_slashes=False)
+@ETapp.route('/earnings/<id>')
+def get_earnings()
