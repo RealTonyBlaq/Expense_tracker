2379824c4846f4353c28c3edbe9080d8a4b9720e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-25 19:28:51 +0100

    earnings route

--- api/v1/routes/earnings.py
+++ api/v1/routes/earnings.py
@@ -3,7 +3,7 @@
 
 from api.v1 import ETapp
 from datetime import datetime
-from flask import request
+from flask import request, jsonify
 from flask_login import login_required, current_user
 from models.earning import Earning
 from utilities import db
@@ -18,5 +18,7 @@ date_format = "%Y-%m-%d"
 def get_earnings(earning_id=None):
     """ Returns an object containing a list of Earning objects """
     if request.is_json:
-        if id:
-            earning = db.query(Earning).filter_by(id =)
+        if earning_id:
+            earning = db.query(Earning).filter_by(id = earning_id).first()
+            if earning is not None:
+                return jsonify({'message': earning.to_dict()})
