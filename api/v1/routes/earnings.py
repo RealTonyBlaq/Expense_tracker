c125544605bd16c3437ca4be5606e8e8e7a3dc71
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-25 19:25:49 +0100

    earnings route

--- api/v1/routes/earnings.py
+++ api/v1/routes/earnings.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from flask import request
 from flask_login import login_required, current_user
 from models.earning import Earning
+from utilities import db
 
 
 date_format = "%Y-%m-%d"
@@ -13,9 +14,9 @@ date_format = "%Y-%m-%d"
 
 @login_required
 @ETapp.route('/earnings', strict_slashes=False)
-@ETapp.route('/earnings/<id>', strict_slashes=False)
-def get_earnings(id=None):
+@ETapp.route('/earnings/<earning_id>', strict_slashes=False)
+def get_earnings(earning_id=None):
     """ Returns an object containing a list of Earning objects """
     if request.is_json:
         if id:
-            
+            earning = db.query(Earning).filter_by(id =)
