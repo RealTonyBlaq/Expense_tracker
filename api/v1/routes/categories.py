e40be5ff6b859901d3904ed8d6c1de60a2574bd5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-08 21:09:07 +0100

    category route

--- api/v1/routes/categories.py
+++ api/v1/routes/categories.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 """ The Category Route """
 
-from api.v1b
+from api.v1 import ETapp
 from flask_jwt_extended import jwt_required, get_current_user
 from models.category import Category
 from utilities import db
+
+
+@ETapp.route('/categories', strict_slashes=False)
+@ETapp.route('/categories/<category_id>', strict_slashes=False)
+def get_categories(category_id):
+    """ Returns a list of categories with the associating Expenses """
+    if category_id:
+        category = db.query(Category).filter_by(id = )
