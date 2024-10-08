292c78242f399c1dc8d0d4128f96e2da3462267e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-08 21:06:04 +0100

    category route

--- api/v1/routes/categories.py
+++ api/v1/routes/categories.py
@@ -1,1 +1,7 @@
-#!
+#!/usr/bin/env python3
+""" The Category Route """
+
+from api.v1b
+from flask_jwt_extended import jwt_required, get_current_user
+from models.category import Category
+from utilities import db
