5d030be8fd769c22bc230c23ea88c482de9642b4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-12 23:49:01 +0100

    update: imported the expenses route

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -10,3 +10,4 @@ ETapp = Blueprint('ETapp', __name__, url_prefix='/api/v1')
 from api.v1.routes.auth import *
 from api.v1.routes.earnings import *
 from api.v1.routes.categories import *
+from api.v1.routes.expenses import *
