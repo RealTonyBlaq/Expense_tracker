96a1063443016eb452beca126ebef6c05daafb0f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-12 21:16:09 +0100

    expense route

--- api/v1/routes/expenses.py
+++ api/v1/routes/expenses.py
@@ -2,5 +2,7 @@
 """ The Expense Route """
 
 from api.v1 import ETapp
+from models.category import Category
 from models.expense import Expense
-
+from flask_jwt_extended import get_current_user, jwt_required
+from werkzeug.exceptions import BadRequest, BadRequestKeyError
