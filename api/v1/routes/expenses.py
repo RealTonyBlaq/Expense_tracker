fcf07c9142896c37d20ad5817cfa7eb10e4ceee6
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-12 21:30:20 +0100

    expense route

--- api/v1/routes/expenses.py
+++ api/v1/routes/expenses.py
@@ -4,6 +4,7 @@
 from api.v1 import ETapp
 from models.category import Category
 from models.expense import Expense
+from flask import abort, jsonify, request
 from flask_jwt_extended import get_current_user, jwt_required
 from werkzeug.exceptions import BadRequest, BadRequestKeyError
 
@@ -12,4 +13,10 @@ from werkzeug.exceptions import BadRequest, BadRequestKeyError
 @ETapp.route('/expenses/<expense_id>', strict_slashes=False)
 @jwt_required()
 def get_expenses(expense_id):
-    """ Retrieves a JSON representation """
+    """ Retrieves a JSON representation of expense objects """
+    current_user = get_current_user()
+    if not current_user or not current_user.is_authenticated:
+        abort(401)
+
+    if expense_id:
+        
