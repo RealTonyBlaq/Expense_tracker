7aef7ee4b0b9a605c580a9699cc28bdac9b9a57a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-12 21:27:15 +0100

    expense route

--- api/v1/routes/expenses.py
+++ api/v1/routes/expenses.py
@@ -8,4 +8,8 @@ from flask_jwt_extended import get_current_user, jwt_required
 from werkzeug.exceptions import BadRequest, BadRequestKeyError
 
 
-@ETapp.route()
+@ETapp.route('/expenses', sstrict_slashes=False)
+@ETapp.route('/expenses/<expense_id>', strict_slashes=False)
+@jwt_required()
+def get_expenses(expense_id):
+    """ Retrieves a JSON representation """
