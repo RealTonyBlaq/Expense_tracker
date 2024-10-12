bcad7db5295f54e4a3046084deac175769b96497
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-12 22:27:20 +0100

    expense route: POST, PUT DELETE

--- api/v1/routes/expenses.py
+++ api/v1/routes/expenses.py
@@ -2,12 +2,18 @@
 """ The Expense Route """
 
 from api.v1 import ETapp
+from datetime import datetime
+from dotenv import load_dotenv, find_dotenv
 from models.category import Category
 from models.expense import Expense
 from flask import abort, jsonify, request
 from flask_jwt_extended import get_current_user, jwt_required
 from utilities import cache, db
 from werkzeug.exceptions import BadRequest, BadRequestKeyError
+
+
+date_format = "%Y-%m-%d"
+load_dotenv(find_dotenv())
 
 
 @ETapp.route('/expenses', sstrict_slashes=False)
@@ -51,7 +57,13 @@ def create_expense(category_id):
         if 'date_occurred' not in data:
             return jsonify(message='date_occurred missing'), 400
 
-        amount = data.get('amount')
         try:
-            
+            amount = int(data.get('amount'))
+        except (ValueError, TypeError):
+            return jsonify(message='Amount must be an integer'), 400
+
+        try:
+            date_occurred = datetime.strptime(data.get('date_occurred'), date_format)
+        except (ValueError, TypeError) as e:
+            return jsonify(message="Invalid date format. Please use '%Y-%m-%d'"), 400
     return jsonify(message='Not a valid JSON'), 400
