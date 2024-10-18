cdabee34f089acb88efef1f6cabe27db964e8368
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-18 20:01:43 +0100

    recurring expense route

--- api/v1/routes/recurring_expenses.py
+++ api/v1/routes/recurring_expenses.py
@@ -6,8 +6,18 @@ from datetime import datetime
 from flask import abort, jsonify
 from flask_jwt_extended import get_current_user, jwt_required
 from models.recurring_expense import RecurringExpense
+from utilities import db
 from werkzeug.exceptions import BadRequest
 
 
 @ETapp.route('/recurring_expenses', strict_slashes=False)
-@ETapp.route('/recurring_expenses/<id')
+@ETapp.route('/recurring_expenses/<id>', strict_slashes=False)
+@jwt_required()
+def get_recurring_expense(id=None):
+    """ Returns the current_user's recurring expenses """
+    current_user = get_current_user()
+    if not current_user or not current_user.is_authenticated:
+        abort(401)
+
+    if id:
+        
