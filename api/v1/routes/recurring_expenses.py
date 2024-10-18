15810e4329cf5b4b5c8943992a188e3ab2efafb5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-18 19:58:41 +0100

    recurring expense route

--- api/v1/routes/recurring_expenses.py
+++ api/v1/routes/recurring_expenses.py
@@ -3,6 +3,11 @@
 
 from api.v1 import ETapp
 from datetime import datetime
+from flask import abort, jsonify
 from flask_jwt_extended import get_current_user, jwt_required
 from models.recurring_expense import RecurringExpense
-from werkzeug.exceptions import 
+from werkzeug.exceptions import BadRequest
+
+
+@ETapp.route('/recurring_expenses', strict_slashes=False)
+@ETapp.route('/recurring_expenses/<id')
