c5bbc2db1a8e70ed18f7bea56f78927007020372
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-19 15:13:13 +0100

    update

--- api/v1/routes/recurring_expenses.py
+++ api/v1/routes/recurring_expenses.py
@@ -125,5 +125,8 @@ def update_recurring_expense(id):
 
         rec_expense = db.query(RecurringExpense).filter_by(id = id, user_id = current_user.id).first()
         if rec_expense is None:
-            ab
+            abort(404)
+
+        for key, value in data:
+            if key in ['amount', 'start_date', 'fequency']
     return jsonify(message='Not a valid JSON'), 400
