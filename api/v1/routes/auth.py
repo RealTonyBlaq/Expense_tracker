c34bd8520976ea9aa2da253ba198ac298dd9b9a8
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-20 20:10:20 +0100

    fix

--- api/v1/routes/auth.py
+++ api/v1/routes/auth.py
@@ -39,10 +39,14 @@ OTP_TIMEOUT = int(getenv('OTP_TIMEOUT'))
 def signup():
     """ Creates a User account """
     if request.is_json:
-        data = request.get_json()
+        try:
+            data = request.get_json()
+        except BadRequest:
+            return jsonify(message='Error parsing JSON data'), 400
+
         for key in ['first_name', 'last_name', 'email', 'password', 'confirm_password']:
             if not data.get(key):
-                return jsonify({'message': f'{key} missing'}), 400
+                return jsonify(message=f'{key} missing'), 400
 
         email = data.get('email').strip()
         first_name = data.get('first_name').strip()
@@ -53,13 +57,13 @@ def signup():
         # Checks if the user exists
         try:
             existing_user = db.get_user(email)
-            return jsonify({'message': 'user already exists'}), 400
+            return jsonify(message='user already exists'), 400
         except ValueError:
             pass
 
         # checks if both passwords match
         if password != confirm_password:
-            return jsonify({'message': 'password mismatch'}), 400
+            return jsonify(message='password mismatch'), 400
 
         user_data = {
             'first_name': first_name,
@@ -105,9 +109,9 @@ def signup():
         key = f'auth_{OTP}'
         cache.set(key, user.email, OTP_TIMEOUT)
 
-        return jsonify({'message': 'user created successfully. Check your inbox for the OTP'}), 201
+        return jsonify(message='user created successfully. Check your inbox for the OTP'), 201
 
-    return jsonify({'message': 'Not a valid JSON'}), 400
+    return jsonify(message='Not a valid JSON'), 400
 
 
 @ETapp.route('/login', methods=['POST', 'GET'], strict_slashes=False)
@@ -115,27 +119,30 @@ def login():
     """ Logs a user in and creates a session"""
     if request.method == 'POST':
         if request.is_json:
-            data = request.get_json()
+            try:
+                data = request.get_json()
+            except BadRequest:
+                return jsonify(message='Error parsing JSON data'), 400
 
             if not data.get('email'):
-                return jsonify({'message': 'email missing'}), 400
+                return jsonify(message='email missing'), 400
 
             if not data.get('password'):
-                return jsonify({'message': 'password missing'}), 400
+                return jsonify(message='password missing'), 400
 
-            email = data.get('email').strip()
+            email = data.get('email').strip().lower()
             password = data.get('password').strip()
 
             try:
                 user = db.get_user(email)
             except ValueError:
-                return jsonify({'message': 'User not found'}), 404
+                return jsonify(message='User not found'), 404
 
             if not checkpw(password.encode('utf-8'), user.password):
-                return jsonify({"message": "Incorrect password"}), 401
+                return jsonify(message="Incorrect password"), 401
 
             if not user.is_active:
-                return jsonify({'message': 'Please verify your email to login'}), 401
+                return jsonify(message='Please verify your email to login'), 401
 
             if user.is_2fa_enabled:
                 OTP = generate_otp()
@@ -156,7 +163,7 @@ def login():
 
                 OTP expires after {OTP_TIMEOUT} minutes.
 
-                If you did not initiate a login attempt, please ignore this email or contact us.
+                If you did not initiate a login attempt, please contact us.
 
                 Thank you!
 
@@ -167,17 +174,17 @@ def login():
                 # Cache the OTP
                 key = f'auth_{OTP}'
                 cache.set(key, user.email, OTP_TIMEOUT)
-                return jsonify({'message': 'OTP sent successfully'}), 200
+                return jsonify(message='OTP sent successfully'), 200
 
             access_token = create_access_token(identity=user)
             user.last_login_time = datetime.now()
             user.is_logged_in = True
             user.save()
-            response = jsonify({'message': 'login successful', 'user': user.to_dict()})
+            response = jsonify(message='login successful', user=user.to_dict())
             set_access_cookies(response, access_token)
             return response, 200
 
-        return jsonify({'message': 'Not a JSON'}), 400
+        return jsonify(message='Not a JSON'), 400
 
 
 @ETapp.route('/logout', strict_slashes=False)
@@ -186,12 +193,12 @@ def logout():
     """ Logs a user out from the session """
     current_user = get_current_user()
     if not current_user or not current_user.is_authenticated:
-        return jsonify({'message': 'user not logged in'}), 401
+        return jsonify(message='user not logged in'), 401
 
     current_user.is_logged_in = False
     current_user.save()
 
-    response = jsonify({'message': 'User logged out successfully'})
+    response = jsonify(message='User logged out successfully')
     unset_jwt_cookies(response)
     return response, 200
 
@@ -202,49 +209,56 @@ def reset():
     """ Resets a user's password """
     if request.method == 'PATCH':
         if request.is_json:
-            data = request.get_json()
+            try:
+                data = request.get_json()
+            except BadRequest:
+                return jsonify(message='Error parsing JSON data'), 400
+
             for key in ['email', 'new_password', 'confirm_password', 'OTP']:
                 if key not in data:
-                    return jsonify({'message': f'{key} missing'}), 400
+                    return jsonify(message=f'{key} missing'), 400
 
-            email = data.get('email').lower()
-            new_password = data.get('new_password')
-            confirm_password = data.get('confirm_password')
+            email = data.get('email').lower().strip()
+            new_password = data.get('new_password').strip()
+            confirm_password = data.get('confirm_password').strip()
             OTP = data.get('OTP')
 
             key = f'auth_{OTP}'
             derived_email = cache.get(key)
             if not derived_email or derived_email != email:
-                return jsonify({'message': 'The OTP is invalid or expired'}), 400
+                return jsonify(message='The OTP is invalid or expired'), 400
 
             if new_password != confirm_password:
-                return jsonify({'message': 'password mismatch'}), 400
+                return jsonify(message='password mismatch'), 400
 
             try:
                 existing_user = db.get_user(email)
             except ValueError:
-                return jsonify({'message': 'Invalid user email'}), 400
+                return jsonify(message='Invalid user email'), 400
 
             existing_user.password = hash_password(new_password)
             existing_user.save()
             cache.delete(key)
-            return jsonify({'message': 'Password changed successfully'}), 200
+            return jsonify(message='Password changed successfully'), 200
 
-        return jsonify({'message': 'Not a valid JSON'}), 400
+        return jsonify(message='Not a valid JSON'), 400
 
     if request.method == 'POST':
         if request.is_json:
-            data = request.get_json()
+            try:
+                data = request.get_json()
+            except BadRequest:
+                return jsonify(message='Error parsing JSON data'), 400
 
             # code to create a token and cache it for 5 minutes
             email = data.get('email')
             if not email:
-                return jsonify({'message': 'email missing'}), 400
+                return jsonify(message='email missing'), 400
 
             try:
-                user = db.get_user(email.lower())
+                user = db.get_user(email.lower().strip())
             except ValueError:
-                return jsonify({'message': 'User not found'}), 400
+                return jsonify(message='User not found'), 400
 
             OTP = generate_otp()
 
@@ -275,9 +289,9 @@ def reset():
             key = f'auth_{OTP}'
             cache.set(key, email, OTP_TIMEOUT)
 
-            return jsonify({'message': 'OTP sent. Please check your email inbox'}), 200
+            return jsonify(message='OTP sent. Please check your email inbox'), 200
 
-        return jsonify({'message': 'Not a Valid JSON'}), 400
+        return jsonify(message='Not a Valid JSON'), 400
 
 
 @ETapp.route('/resend_otp/<process>', strict_slashes=False)
@@ -378,29 +392,30 @@ def verify_otp(process, otp):
         return jsonify(message='Email parameter missing'), 400
 
     if not email or email != email_by_param:
-        return jsonify({'message': 'Invalid OTP, try again'}), 400
+        return jsonify(message='Invalid OTP, try again'), 400
 
     try:
         user = db.get_user(email)
     except ValueError:
-        return jsonify({'message': 'Invalid OTP, try again'}), 400
+        return jsonify(message='Invalid OTP, try again'), 400
 
     if process == 'signup':
         if user.is_email_verified:
-            return jsonify({'message': 'Email already verified'}), 400
+            return jsonify(message='Email already verified'), 400
+
         user.is_email_verified = True
         user.save()
         cache.delete(key)
-        return jsonify({'message': 'Email verification successful'}), 200
+        return jsonify(message='Email verification successful'), 200
     elif process == '2fa':
         user.last_login_time = datetime.now()
         user.is_email_verified = True
         user.is_logged_in = True
         user.save()
         cache.delete(key)
-        response = jsonify({'message': 'login successful', 'user': user.to_dict()})
+        response = jsonify(message='login successful', user=user.to_dict())
         access_token = create_access_token(identity=user)
         set_access_cookies(response, access_token)
         return response, 200
 
-    return jsonify({'message': 'Invalid process type'}), 401
+    return jsonify(message='Invalid process type'), 401
