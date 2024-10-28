15d89eeb9ede811b2e6c8846d6d9e52999775713
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-28 13:24:48 +0100

    self - current user route

--- api/v1/routes/self.py
+++ api/v1/routes/self.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 """ The current user route """
 
+from api.v1 import ETapp
+from datetime import datetime
+from flask import abort, jsonify, request
+from flask_jwt_extended import jwt_required, get_current_user
+from werkzeug.exceptions import BadRequest
+
+
+@ETapp.route('/me', strict_slashes=False)
+
