58c13b79fa60d0496bb368d46a6dc115e5e2bbac
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-11-12 16:12:25 +0100

    update

--- utilities/statement.py
+++ utilities/statement.py
@@ -35,5 +35,4 @@ class Statement:
         df_part1 = df.iloc[:empty_row_index + 1]  # Including the empty row
         df_part2 = df.iloc[empty_row_index + 1:]  # Remaining rows after the empty row
 
-        df = pd.concat([df_part1, df2, df_part2])
         return [df, df2]
