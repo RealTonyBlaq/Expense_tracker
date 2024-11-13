f770e199d845a4cb7424b24f657f32291934ff99
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-11-13 19:08:43 +0100

    update

--- utilities/email.py
+++ utilities/email.py
@@ -7,20 +7,18 @@ from yagmail.error import YagInvalidEmailAddress, YagAddressError
 
 class Email:
     """ Defining the Email class """
-    from api.v1.app import app
 
-    dev_email = app.config['MAIL_DEFAULT_SENDER']
-    dev_password = app.config['MAIL_PASSWORD']
-
+    def __init__(self):
+        """ Inintializes the variables """
+        from api.v1.app import app
+        dev_email = app.config['MAIL_DEFAULT_SENDER']
+        dev_password = app.config['MAIL_PASSWORD']
+        self.connect = yagmail.SMTP(dev_email, dev_password)
 
     @classmethod
     def send(self, email: str, subject: str, content: str) -> bool:
         """ sends a confirmation email with token """
-        
-
         if email and subject and content:
-            connect = yagmail.SMTP(dev_email, dev_password)
-
             try:
                 connect.send(email, subject=subject, contents=content)
                 connect.close()
