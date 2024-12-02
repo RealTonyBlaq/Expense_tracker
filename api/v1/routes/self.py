42120304161061e51e2efecb62720511041bc783
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-12-02 14:59:22 +0100

    adding a scan receipt endpoint

--- api/v1/routes/self.py
+++ api/v1/routes/self.py
@@ -169,4 +169,10 @@ def scan_receipt():
     current_user = get_current_user()
     if not current_user or not current_user.is_authenticated:
         abort(401)
-    
+
+    if 'image' not in request.files:
+        return jsonify(message='No image found in the request'), 400
+
+    file = request.files['image']
+    if not file or file.filename == '':
+        return jsonify(message='No file selected'), 400
