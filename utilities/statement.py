2765d48fe04c915f755eb4c49ca9ede4c2931f5c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-12-18 21:31:57 +0100

    update

--- utilities/statement.py
+++ utilities/statement.py
@@ -3,15 +3,29 @@
 
 import pandas as pd
 from models.user import User
+from typing import Dict
 
 
 class Statement:
     """ The statement class """
 
     @classmethod
-    def get_html(self, user: User):
+    def get_html_statement(self, user: User, period:Dict[str, str] = None):
         """ Returns the Excel workbook """
-        txns = user.generate_statement()
+        txns = user.generate_statement(period)
+        money_in, money_out = 0.0, 0.0
+
+        for t in txns:
+            if t['Type'] == 'Earning':
+                money_in += t['Amount']
+            else:
+                money_out += t['Amount']
+
+        summary = {'Total_credit': money_in,
+                   'Total_debit': money_out,
+                   'Balance': money_in - money_out
+        }
+
         df = pd.DataFrame({
             'Date of Transaction': [d['Date_occurred'] for d in txns],
             'Description': [des['Description'] for des in txns],
@@ -19,4 +33,4 @@ class Statement:
             'Amount': [a['Amount'] for a in txns]
         })
 
-        return df.to_html(index=False, classes='styled-table', border=0)
+        return df.to_html(index=False, classes='styled-table', border=0), summary
